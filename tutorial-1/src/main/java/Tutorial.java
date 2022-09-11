c82cda65c95377effd12c5a3a9e36f9b8c8ebe18
Sean Levin <slevin@yahooinc.com>
2022-09-11 11:52:21 -0700

    formatting

--- tutorial-1/src/main/java/Tutorial.java
+++ tutorial-1/src/main/java/Tutorial.java
@@ -3,62 +3,62 @@ import com.yahoo.behaviorgraph.*;
 import java.util.List;
 
 public class Tutorial {
-    public static void main(String[] args) {
-        Graph g = new Graph();
-        TutorialExtent e = new TutorialExtent(g);
+  public static void main(String[] args) {
+    Graph g = new Graph();
+    TutorialExtent e = new TutorialExtent(g);
 
-        e.addToGraphWithAction();
+    e.addToGraphWithAction();
 
-        g.action(() -> {
-            e.person.update("World");
-            e.greeting.update("Hello");
-        });
-        g.action(() -> {
-            e.greeting.update("Goodbye");
-        });
-        g.action(() -> {
-            e.button.update();
-            e.greeting.update("Nevermind");
-            e.loggingEnabled.update(false);
-        });
-    }
+    g.action(() -> {
+      e.person.update("World");
+      e.greeting.update("Hello");
+    });
+    g.action(() -> {
+      e.greeting.update("Goodbye");
+    });
+    g.action(() -> {
+      e.button.update();
+      e.greeting.update("Nevermind");
+      e.loggingEnabled.update(false);
+    });
+  }
 }
 
 class TutorialExtent extends Extent<TutorialExtent> {
 
-    State<String> person = state("Nobody");
-    State<String> greeting = state("Greetings");
-    Moment button = moment();
-    State<String> message = state(null);
-    Moment sentMessage = moment();
-    State<Boolean> loggingEnabled = state(true);
+  State<String> person = state("Nobody");
+  State<String> greeting = state("Greetings");
+  Moment button = moment();
+  State<String> message = state(null);
+  Moment sentMessage = moment();
+  State<Boolean> loggingEnabled = state(true);
 
-    public TutorialExtent(Graph g) {
-        super(g);
+  public TutorialExtent(Graph g) {
+    super(g);
 
-        behavior()
-                .supplies(message, sentMessage)
-                .demands(person, greeting, button)
-                .runs(ctx -> {
-                    message.update(greeting.value() + ", " + person.value() + "!");
-                    if (button.justUpdated()) {
-                        System.out.println(message.value());
-                        sentMessage.update();
-                    }
-                });
+    behavior()
+      .supplies(message, sentMessage)
+      .demands(person, greeting, button)
+      .runs(ctx -> {
+        message.update(greeting.value() + ", " + person.value() + "!");
+        if (button.justUpdated()) {
+          System.out.println(message.value());
+          sentMessage.update();
+        }
+      });
 
-        behavior()
-                .demands(message, sentMessage, loggingEnabled)
-                .runs(ctx -> {
-                    if (loggingEnabled.value()) {
-                        if (message.justUpdated()) {
-                            System.out.println("Message changed to: " + message.value() + " : " + message.event().getTimestamp());
-                        }
-                        if (sentMessage.justUpdated()) {
-                            System.out.println("Message sent: " + message.value() + " : " + message.event().getTimestamp());
-                        }
-                    }
-                });
-    }
+    behavior()
+      .demands(message, sentMessage, loggingEnabled)
+      .runs(ctx -> {
+        if (loggingEnabled.value()) {
+          if (message.justUpdated()) {
+            System.out.println("Message changed to: " + message.value() + " : " + message.event().getTimestamp());
+          }
+          if (sentMessage.justUpdated()) {
+            System.out.println("Message sent: " + message.value() + " : " + message.event().getTimestamp());
+          }
+        }
+      });
+  }
 
 }
--- tutorial-2-swing/src/main/java/Thermostat.java
+++ tutorial-2-swing/src/main/java/Thermostat.java
@@ -4,84 +4,84 @@ import com.yahoo.behaviorgraph.Moment;
 import com.yahoo.behaviorgraph.State;
 
 public class Thermostat extends Extent<Thermostat> {
-    State<Integer> desiredTemperature;
-    State<Integer> currentTemperature;
-    State<Boolean> heatOn;
+  State<Integer> desiredTemperature;
+  State<Integer> currentTemperature;
+  State<Boolean> heatOn;
 
-    Moment up;
-    Moment down;
-    TutorialUI ui;
+  Moment up;
+  Moment down;
+  TutorialUI ui;
 
-    public Thermostat(Graph g, TutorialUI uiParam) {
-        super(g);
-        ui = uiParam;
+  public Thermostat(Graph g, TutorialUI uiParam) {
+    super(g);
+    ui = uiParam;
 
-        up = moment();
-        ui.upButton.addActionListener(e -> {
-            up.updateWithAction();
+    up = moment();
+    ui.upButton.addActionListener(e -> {
+      up.updateWithAction();
+    });
+
+    down = moment();
+    ui.downButton.addActionListener(e -> {
+      down.updateWithAction();
+    });
+
+    desiredTemperature = state(60);
+    currentTemperature = state(60);
+    heatOn = state(false);
+
+    // Set the desired Temperature
+    behavior()
+      .supplies(desiredTemperature)
+      .demands(up, down, getDidAdd())
+      .runs(ctx -> {
+        if (up.justUpdated()) {
+          desiredTemperature.update(desiredTemperature.value() + 1);
+        } else if (down.justUpdated()) {
+          desiredTemperature.update(desiredTemperature.value() - 1);
+        }
+        sideEffect(ctx1 -> {
+          ui.desiredTemp.setText(desiredTemperature.value().toString());
         });
+      });
 
-        down = moment();
-        ui.downButton.addActionListener(e -> {
-            down.updateWithAction();
+    // Update current temperature display
+    behavior()
+      .demands(currentTemperature, getDidAdd())
+      .runs(ctx -> {
+        sideEffect(ctx1 -> {
+          ui.currentTemp.setText(currentTemperature.value().toString());
         });
+      });
 
-        desiredTemperature = state(60);
-        currentTemperature = state(60);
-        heatOn = state(false);
+    // Determine if heat is on
+    behavior()
+      .supplies(heatOn)
+      .demands(currentTemperature, desiredTemperature, getDidAdd())
+      .runs(ctx -> {
+        boolean on = desiredTemperature.value() > currentTemperature.value();
+        heatOn.update(on);
+        sideEffect(ctx1 -> {
+          ui.heatStatus.setText("Heat " + (heatOn.value() ? "On" : "Off"));
+        });
+      });
 
-        // Set the desired Temperature
-        behavior()
-                .supplies(desiredTemperature)
-                .demands(up, down, getDidAdd())
-                .runs(ctx -> {
-                    if (up.justUpdated()) {
-                        desiredTemperature.update(desiredTemperature.value() + 1);
-                    } else if (down.justUpdated()) {
-                        desiredTemperature.update(desiredTemperature.value() - 1);
-                    }
-                    sideEffect(ctx1 -> {
-                        ui.desiredTemp.setText(desiredTemperature.value().toString());
-                    });
-                });
+    // Control heating equipment
+    behavior()
+      .demands(heatOn)
+      .runs(ctx -> {
+        if (heatOn.justUpdatedTo(true)) {
+          sideEffect(ctx1 -> {
+            ui.turnOnHeat(e -> {
+              currentTemperature.updateWithAction(currentTemperature.value() + 1);
+            });
+          });
+        } else if (heatOn.justUpdatedTo(false)) {
+          sideEffect(ctx1 -> {
+            ui.turnOffHeat();
+          });
+        }
+      });
 
-        // Update current temperature display
-        behavior()
-                .demands(currentTemperature, getDidAdd())
-                .runs(ctx -> {
-                    sideEffect(ctx1 -> {
-                        ui.currentTemp.setText(currentTemperature.value().toString());
-                    });
-                });
-
-        // Determine if heat is on
-        behavior()
-                .supplies(heatOn)
-                .demands(currentTemperature, desiredTemperature, getDidAdd())
-                .runs(ctx -> {
-                    boolean on = desiredTemperature.value() > currentTemperature.value();
-                    heatOn.update(on);
-                    sideEffect(ctx1 -> {
-                        ui.heatStatus.setText("Heat " + (heatOn.value() ? "On" : "Off"));
-                    });
-                });
-
-        // Control heating equipment
-        behavior()
-                .demands(heatOn)
-                .runs(ctx -> {
-                    if (heatOn.justUpdatedTo(true)) {
-                        sideEffect(ctx1 -> {
-                            ui.turnOnHeat(e -> {
-                                currentTemperature.updateWithAction(currentTemperature.value() + 1);
-                            });
-                        });
-                    } else if (heatOn.justUpdatedTo(false)) {
-                        sideEffect(ctx1 -> {
-                            ui.turnOffHeat();
-                        });
-                    }
-                });
-
-    }
+  }
 }
